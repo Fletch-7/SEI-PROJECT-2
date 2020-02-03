9c387eebaaba57022f9bf692abe26f9202fc836b
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-02-03 15:39:09 +0000

    deployapp

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(express.static(`${__dirname}/dist`))
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+
+app.listen(port, () => console.log(`app is listening on port ${port}`))
+
